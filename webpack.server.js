477a3483e10bff1158a354f5f426a34e53623045
Bew <Bew@sysops-MBP.broadband>
2021-01-26 17:33:56 +0000

    Refactor webpack config file and also use webpack-merge

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const baseConfig = require('./webpack.base.js');
+
+const config = {
   // Inform webpack that we are building a bundle for
   // nodeJS rather than for the browser
   target: 'node',
@@ -14,22 +17,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browser: ['last 2 versions'] } }],
-          ],
-        },
-      },
-    ],
-  },
 };
+
+module.exports = merge(baseConfig, config);
